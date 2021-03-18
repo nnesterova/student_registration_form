0a32bad4b9426cb9b773ac87c21d55fe1d6d7e19
nnesterova <nvnesterova1994@gmail.com>
2021-03-18 21:25:51 +0300

    added my files

---
+++ student_registration_form3/src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,72 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.Keys.ENTER;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void successfulFillTest() {
+
+        String firstName = "Natalia";
+        String lastName = "Nesterova";
+        String userEmail = "nvnesterova@gmail.com";
+        String userNumber = "8902668687";
+        String subjectsInput = "Maths";
+        String currentAddress = "Yaroslavskaya street";
+        String state = "NCR";
+        String city = "Delhi";
+
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        //клик по радиобаттону
+        //сохраню себе два спокоба клика по элементу
+        $(byText("Male")).click();
+        $("label[for='gender-radio-2']").click();
+        $("#userNumber").setValue(userNumber);
+        //клик по полю и следом клик по дню в календаре
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__day.react-datepicker__day--011").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+        $("input#uploadPicture").uploadFile(new File("src/test/resources/avatar.jpg"));
+        $("#currentAddress").setValue("Yaroslavskaya street");
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
+        $("#submit").click();
+
+        $(".modal-content").shouldHave
+                (text(firstName),
+                        text(userNumber),
+                        text(lastName),
+                        text(subjectsInput),
+                        text(userEmail),
+                        text(currentAddress),
+                        text(state),
+                        text(city),
+                        text("avatar.jpg"),
+                        text("11 March,2021"));
+
+    }
+
+
+}
